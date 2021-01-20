9e53e8f87e1d81e5800162a1f8598b4045f22c94
Mathew E Newton <elijah2895@hotmail.com>
2021-01-20 02:09:50 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,7 @@
 def using_until
-  counter = 0
   levitation_force = 6
     until levitation_force == 10
-      counter += 1
+      levitation_force += 1
       puts "Wingardium Leviosa"
     end
 end
